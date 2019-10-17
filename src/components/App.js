74933a8bca21e195f15452e02cf0211c5e403ac8
Michael Brewer <msbrewer@gmail.com>
2019-10-16 19:29:23 -0500

    troubleshooting

--- src/components/App.js
+++ src/components/App.js
@@ -80,7 +80,7 @@ const App = () => {
     <div className="App">
       <Header text="HOOKED" />
       <Search search={search} />
-      <p className="App-intro">Sharing a few of our favourite movies</p>
+      <p className="App-intro">Sharing a few of our favorite movies</p>
       <div className="movies">
         {loading && !errorMessage ? (
           <span>loading... </span>
